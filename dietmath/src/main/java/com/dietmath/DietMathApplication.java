0a2fd5b583b978a72983d4b830b440b610bb35d3
Kiss Attila <attila.kiss@idata.hu>
2025-12-25 13:45:40 +0100

    Create Spring project
    * Java
    * Maven
    * Spring Boot 4.0.1
    * Jar packaging
    * Java 21
    * Dependencies: Spring Web, Spring Data JPA, PostgreSQL Driver, Flyway
      Migration, Validation

---
+++ dietmath/src/main/java/com/dietmath/DietMathApplication.java
@@ -0,0 +1,13 @@
+package com.dietmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietMathApplication.class, args);
+	}
+
+}
